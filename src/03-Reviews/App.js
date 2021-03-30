109111c9f3aeb492e1a7a8ac4efc0322e6c44574
Suvankar Das <iambabai0@gmail.com>
2021-03-30 20:32:48 +0530

    03-Reviews initial setup

---
+++ src/03-Reviews/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Review from './Review';
+
+import './index.css';
+function App() {
+	return <h2>Reviews</h2>;
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import Main from './01-BirthdayReminder/App';
-import Main from './02-Tour/App';
+// import Main from './02-Tour/App';
+import Main from './03-Reviews/App';
 const App = () => {
 	return (
 		<div>

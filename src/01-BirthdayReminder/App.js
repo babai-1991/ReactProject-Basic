e4115ebc5ee7b105354eee39eebbc40a48f5e700
Suvankar Das <iambabai0@gmail.com>
2021-03-30 11:44:29 +0530

    01-Birthday reminder project - initial setup

---
+++ src/01-BirthdayReminder/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+import './index.css';
+function App() {
+	return (
+		<main>
+			<section className="container">
+				<h3>0 Birthdays Today</h3>
+				<List />
+				<button onClick={() => console.log('Clicked')}>Clear All</button>
+			</section>
+		</main>
+	);
+}
+
+export default App;
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Main from './01-BirthdayReminder/App';
+const App = () => {
+	return (
+		<div>
+			<Main />
+		</div>
+	);
+};
+
+export default App;

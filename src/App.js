5b4b776f01e313170309c6670fcfab370d328e1d
Suvankar Das <iambabai0@gmail.com>
2021-03-30 12:19:47 +0530

    02-tour setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Main from './01-BirthdayReminder/App';
+// import Main from './01-BirthdayReminder/App';
+import Main from './02-Tour/App';
 const App = () => {
 	return (
 		<div>

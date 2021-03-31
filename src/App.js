e7a159ffb2fc78011d4cf8a7c6e03968a3eefdd4
Suvankar Das <iambabai0@gmail.com>
2021-03-31 19:11:51 +0530

    07-slider project

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import React from 'react';
 // import Main from './03-Reviews/App';
 // import Main from './04-Accordian/App'
 // import Main from './05-Menu/App';
-import Main from './06-Tabs/App';
+// import Main from './06-Tabs/App';
+import Main from './07-Slider/App';
 const App = () => {
 	return (
 		<div>

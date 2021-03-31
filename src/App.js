de4a8bb0cac45f243668620ec795affb43a26306
Suvankar Das <iambabai0@gmail.com>
2021-03-31 11:32:36 +0530

    04-accordian project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 // import Main from './01-BirthdayReminder/App';
 // import Main from './02-Tour/App';
-import Main from './03-Reviews/App';
+// import Main from './03-Reviews/App';
+import Main from './04-Accordian/App'
 const App = () => {
-	return (
-		<div>
-			<Main />
-		</div>
-	);
+    return (
+        <div>
+            <Main />
+        </div>
+    );
 };
 
 export default App;

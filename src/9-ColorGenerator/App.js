52019ef8c74899984bc44e52434570bad8cf9a77
Suvankar Das <iambabai0@gmail.com>
2021-04-03 04:20:13 +0530

    09-color generator

---
+++ src/9-ColorGenerator/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import SingleColor from './SingleColor';
+import './index.css';
+
+// import Values from 'values.js';
+
+function App() {
+	return <h2>color generator project</h2>;
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import React from 'react';
 // import Main from './05-Menu/App';
 // import Main from './06-Tabs/App';
 // import Main from './07-Slider/App';
-import Main from './8-Lorem/App';
+// import Main from './8-Lorem/App';
+import Main from './9-ColorGenerator/App';
 const App = () => {
 	return (
 		<div>

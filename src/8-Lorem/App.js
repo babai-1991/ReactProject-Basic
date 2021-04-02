5b511fe84eb1d9fa598c4ff62e93ebc171c5fc83
Suvankar Das <iambabai0@gmail.com>
2021-04-03 04:04:13 +0530

    8-lorem complete

--- src/8-Lorem/App.js
+++ src/8-Lorem/App.js
@@ -1,12 +1,47 @@
 import React, { useState } from 'react';
 import data from './data';
 import './index.css';
+
 function App() {
-  
-  
-  return (
-  <h2>lorem ipsum project setup</h2>
-    )
+	const [ paragraphCount, setParagraphCount ] = useState(0);
+	const [ text, setText ] = useState([]);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		let pCount = parseInt(paragraphCount);
+		// slice will return a new copy of array we select item from (startIndx , endIndx)
+		if (isNaN(pCount) || pCount <= 0) {
+			pCount = 1;
+		}
+		//I have total 9 paragraph in my data.js
+		if (pCount > 9) {
+			pCount = 9;
+		}
+		setText(data.slice(0, pCount));
+	};
+	return (
+		<section className="section-center">
+			<h3>Dummy text? More like dummy thicc text, amirite?</h3>
+			<form className="lorem-form" onSubmit={(e) => handleSubmit(e)}>
+				<label htmlFor="amount">parapgraphs: </label>
+				<input
+					type="number"
+					name="amount"
+					id="amount"
+					value={paragraphCount}
+					onChange={(e) => setParagraphCount(e.target.value)}
+				/>
+				<button type="submit" className="btn">
+					Generate
+				</button>
+			</form>
+			<article className="lorem-text">
+				{text.map((paragraph, index) => {
+					return <p key={index}>{paragraph}</p>;
+				})}
+			</article>
+		</section>
+	);
 }
 
 export default App;

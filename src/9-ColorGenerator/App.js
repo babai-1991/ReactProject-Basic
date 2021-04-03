fb58afce650f1ce5d3a99c1add5dbf4cdc000a5e
Suvankar Das <iambabai0@gmail.com>
2021-04-03 06:18:50 +0530

    09-color picker end

--- src/9-ColorGenerator/App.js
+++ src/9-ColorGenerator/App.js
@@ -1,11 +1,55 @@
 import React, { useState } from 'react';
 import SingleColor from './SingleColor';
 import './index.css';
-
-// import Values from 'values.js';
+// imported external Values.js libaray ⮦⮦
+import Values from 'values.js';
 
 function App() {
-	return <h2>color generator project</h2>;
+	const [ color, setColor ] = useState('');
+	const [ error, setError ] = useState(false);
+	//clean ui
+	//const [ colorList, setColorList ] = useState([]);
+	//populate with predefined color
+	const [ colorList, setColorList ] = useState(new Values('#f41853').all(10));
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		// if invalid color entered, then Values.js will throw an exception.
+		try {
+			let colors = new Values(color).all(10); // Values()-> from library , all() returns array
+			//console.log(colors);
+			setColorList(colors);
+			setError(false);
+		} catch (error) {
+			//console.log(error);
+			setError(true);
+		}
+	};
+	return (
+		<React.Fragment>
+			<section className="container">
+				<h3>color generator</h3>
+				<form onSubmit={(e) => handleSubmit(e)}>
+					<input
+						type="text"
+						value={color}
+						onChange={(e) => setColor(e.target.value)}
+						placeholder="#f41853"
+						className={`${error ? 'error' : null}`}
+					/>
+					<button className="btn" type="submit">
+						Generate
+					</button>
+				</form>
+			</section>
+			<section className="colors">
+				{colorList.map((color, index) => {
+					console.log(color);
+					return <SingleColor key={index} {...color} index={index} hexColor={color.hex} />;
+				})}
+			</section>
+		</React.Fragment>
+	);
 }
 
 export default App;
--- src/9-ColorGenerator/SingleColor.js
+++ src/9-ColorGenerator/SingleColor.js
@@ -1,8 +1,44 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from 'react';
+import rgbToHex from './utils';
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+	//console.log(rgb);
+	// rgb is an array
+	const [ copiedToClipBoard, setCopiedToClipBoard ] = useState(false);
+	const bcg = rgb.join(',');
+	const hexFromRgb = rgbToHex(...rgb);
+	const hexValue = `#${hexColor}`;
 
-export default SingleColor
+	const copyToClipBoard = () => {
+		setCopiedToClipBoard(true);
+		navigator.clipboard.writeText(hexValue);
+	};
+	useEffect(
+		() => {
+			const useForClear = setTimeout(() => {
+				setCopiedToClipBoard(false);
+			}, 3000);
+			//before I set up another timeout, let me clear out the first one.
+			return () => {
+				clearTimeout(useForClear);
+			};
+		},
+		[ copiedToClipBoard ]
+	);
+	return (
+		// for better readibility for darker background add light color, use below code🡣
+		// className={`color ${index > 10 && 'color-light'}`}
+		<article
+			className={`color ${index > 10 && 'color-light'}`}
+			style={{ background: `rgb(${bcg})` }}
+			onClick={() => copyToClipBoard()}
+		>
+			<p className="percent-value">{weight}%</p>
+			{/* <p className="color-value">{hexFromRgb}</p> */}
+			<p className="color-value">{hexValue}</p>
+			{copiedToClipBoard && <p className="alert">Copied to clipboard</p>}
+		</article>
+	);
+};
+
+export default SingleColor;
